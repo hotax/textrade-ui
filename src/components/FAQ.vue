1af11f797fa28370f3ab1073568e1a90a70186b4
clx1608@qq.com <hbdennischen@outlook.com>
2018-06-01 16:02:26 +0800

    load data

--- src/components/FAQ.vue
+++ src/components/FAQ.vue
@@ -1,5 +1,31 @@
 <template>
     <main class="faq">
         <h1>Frenquently Asked Questions</h1>
+        <div class="error" v-if="error">
+            Can't load the questions
+        </div>
+        <section class="list">
+            <article v-for="question of questions">
+                <h2 v-html="question.title"></h2>
+                <p v-html="question.content"></p>
+            </article>
+        </section>
     </main>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                questions: [{
+                    title: 'title1',
+                    content: 'content of question 1'
+                }, {
+                    title: 'title2',
+                    content: 'content of question2'
+                }],
+                error: null,
+            }
+        },
+    }
+</script>
