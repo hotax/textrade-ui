73aeb0e8cffb72942779585aaff9ae1ed7496ebe
clx1608@qq.com <hbdennischen@outlook.com>
2018-06-01 18:44:56 +0800

    add RemoteData mixins to reusing code

---
+++ src/mixins/RemoteData.js
@@ -0,0 +1,49 @@
+export default function(resources) {
+    return {
+        data() {
+            let initData = {
+                remoteDataLoading: 0,
+                remoteErrors: {}
+            }
+            for (const key in resources) {
+                initData[key] = null
+                initData.remoteErrors[key] = null
+            }
+            return initData
+        },
+        computed: {
+            remoteDataBusy() {
+                return this.$data.remoteDataLoading !== 0
+            },
+
+            hasRemoteErrors() {
+                return Object.keys(this.$data.remoteErrors).some(
+                    key => this.$data.remoteErrors[key]
+                )
+            },
+        },
+        methods: {
+            fetchResource(key, url) {
+                this.$data.remoteDataLoading++;
+                this.$data.remoteErrors[key] = null;
+
+                return this.$fetch(url)
+                    .then(d => {
+                        this.$data[key] = d
+                        this.$data.remoteDataLoading--
+                    })
+                    .catch(e => {
+                        console.error(e);
+                        this.$data.remoteErrors[key] = e
+                        this.$data.remoteDataLoading--
+                    })
+            },
+        },
+        created() {
+            for (const key in resources) {
+                let url = resources[key]
+                this.fetchResource(key, url)
+            }
+        },
+    }
+}
