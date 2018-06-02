debdbc7a0b3920085e04621201ead9eeaef067af
clx1608@qq.com <hbdennischen@outlook.com>
2018-06-02 19:31:08 +0800

    Logout

--- src/plugins/fetch.js
+++ src/plugins/fetch.js
@@ -1,38 +1,54 @@
-function listQuestions() {
-    return Promise.resolve([{
-        title: 'title1',
-        content: 'content of question 1'
-    }, {
-        title: 'title2',
-        content: 'content of question2'
-    }])
+function listQuestions () {
+  return Promise.resolve([
+    {
+      title: 'title1',
+      content: 'content of question 1'
+    },
+    {
+      title: 'title2',
+      content: 'content of question2'
+    }
+  ])
 }
 
-function login(req) {
-    req.a = 1
-    return Promise.resolve({})
+function login (req) {
+  let body = JSON.parse(req.body)
+  return Promise.resolve({
+    username: body.username
+  })
+}
+
+function logout () {
+  return Promise.resolve({
+    status: 'ok'
+  })
 }
 
 const resourcesFetches = {
-    'http://localhost:3000/questions': listQuestions,
-    'http://localhost:3000/login': login
+  'http://localhost:3000/questions': listQuestions,
+  'http://localhost:3000/login': login,
+  'http://localhost:3000/logout': logout
 }
 
 let baseUrl
 
-function $fetch(url, options) {
-    const finalOptions = Object.assign({}, {
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        credentials: 'include'
-    }, options)
-    return resourcesFetches[`${baseUrl}${url}`](finalOptions)
+function $fetch (url, options) {
+  const finalOptions = Object.assign(
+        {},
+    {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      credentials: 'include'
+    },
+        options
+    )
+  return resourcesFetches[`${baseUrl}${url}`](finalOptions)
 }
 
 export default {
-    install(Vue, options) {
-        baseUrl = options.baseUrl
-        Vue.prototype.$fetch = $fetch
-    }
+  install (Vue, options) {
+    baseUrl = options.baseUrl
+    Vue.prototype.$fetch = $fetch
+  }
 }
