afd82ea77ccd0ccc9d3a3d0d5ec6f1004aadd7f4
clx1608@qq.com <hbdennischen@outlook.com>
2018-06-03 17:07:24 +0800

    Sending a form

--- src/components/NewTicket.vue
+++ src/components/NewTicket.vue
@@ -1,5 +1,28 @@
 <template>
-    <div class="new-ticket">
-        <h1>New ticket</h1>
-    </div>
+    <SmartForm title="New ticket" :operation="operation" :valid="valid">
+        <FormInput name="title" v-model="title" placeholder="Short description (max 100 chars)"
+            maxlength="100" required/>
+    </SmartForm>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                title: '',
+                description: '',
+            }
+        },
+        computed: {
+            valid() {
+                return !!this.title && !!this.description
+            },
+        },
+        methods: {
+            async operation() {
+                // TODO
+            },
+        },
+    }
+
+</script>
