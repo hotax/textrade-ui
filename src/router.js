c06370e095a53f07aebfe6a3f259e739c5e5add8
clx1608@qq.com <hbdennischen@outlook.com>
2018-06-03 16:47:18 +0800

    use the some array method to verify if at least one route object has the desired meta attribute

--- src/router.js
+++ src/router.js
@@ -55,7 +55,7 @@ const router = new VueRouter({
 });
 
 router.beforeEach((to, from, next) => {
-	if (to.meta.private && !state.user) {
+	if (to.matched.some(r => r.meta.private) && !state.user) {
 		next({
 			name: 'login',
 			params: {
@@ -64,7 +64,7 @@ router.beforeEach((to, from, next) => {
 		});
 		return;
 	}
-	if (to.meta.guest && state.user) {
+	if (to.matched.some(r => r.meta.guest) && state.user) {
 		next({
 			name: 'home'
 		});
