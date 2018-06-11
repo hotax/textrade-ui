b920968c5c3b6ab575478f34250bf1cb9da185d1
clx1608@qq.com <hbdennischen@outlook.com>
2018-06-11 11:56:45 +0800

    auth with github account

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -9,11 +9,35 @@
     </div>
 </template>
 <script>
+    import {
+        mapActions
+    } from 'vuex'
     export default {
         methods: {
+            ...mapActions(['login', ]),
             openGoogleSignin() {
-                // TODO
+                const url = 'http://localhost:8089/api/auth/github'
+                const name = 'github_login'
+                //const url = 'http://localhost:8089/api/auth/google'
+                //const name = 'google_login'
+                const
+                    specs = 'width=500,height=500'
+                window.open(url, name, specs)
             },
+            handleMessage({
+                data,
+                origin
+            }) {
+                if (origin !== 'http://localhost:8089') {
+                    return
+                }
+                if (data === 'success') {
+                    this.login()
+                }
+            },
+        },
+        mounted() {
+            window.addEventListener('message', this.handleMessage)
         },
     }
 </script>
